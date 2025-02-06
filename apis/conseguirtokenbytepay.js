db36ab419bd8b20347bce6e843c21031fd04af71
Renan Ferrari <134348963+devrenanferrari@users.noreply.github.com>
2025-02-05 22:28:03 -0300

    Update conseguirtokenbytepay.js

--- apis/conseguirtokenbytepay.js
+++ apis/conseguirtokenbytepay.js
@@ -1,36 +1,35 @@
 const { Client } = require('pg');
 
-// Função para atualizar o bytepayToken no banco de dados
 async function atualizarTokenBytepay(bytepayToken, userEmail) {
-    const connectionString = 'postgresql://postgres:yTIfpgaftZdLNzahJvJlVNpMoAlwInbL@monorail.proxy.rlwy.net:12369/railway';
-    const client = new Client({ connectionString });
+  const connectionString = 'postgresql://postgres:yTIfpgaftZdLNzahJvJlVNpMoAlwInbL@monorail.proxy.rlwy.net:12369/railway';
+  const client = new Client({ connectionString });
 
-    try {
-        await client.connect();
-        console.log('Conectado ao banco de dados!');
+  try {
+    await client.connect();
+    console.log('Conectado ao banco de dados!');
 
-        const query = `
-            UPDATE users 
-            SET bytepaytoken = $1 
-            WHERE email = $2 
-            RETURNING *;
-        `;
+    const query = `
+      UPDATE users 
+      SET bytepaytoken = $1 
+      WHERE email = $2 
+      RETURNING *;
+    `;
 
-        const res = await client.query(query, [bytepayToken, userEmail]);
+    const res = await client.query(query, [bytepayToken, userEmail]);
 
-        if (res.rowCount > 0) {
-            console.log(`BytePayToken atualizado com sucesso para o usuário ${userEmail}`);
-            return { success: true, message: 'Token atualizado com sucesso!' };
-        } else {
-            console.log(`Nenhum usuário encontrado com o e-mail: ${userEmail}`);
-            return { success: false, message: 'Usuário não encontrado' };
-        }
-    } catch (err) {
-        console.error('Erro ao atualizar o bytepaytoken:', err);
-        return { success: false, message: 'Erro ao atualizar o token no banco de dados' };
-    } finally {
-        await client.end();
+    if (res.rowCount > 0) {
+      console.log(`Token atualizado para o usuário ${userEmail} com sucesso!`);
+      return { success: true, message: 'Integração com Adquirente realizada com sucesso!' };
+    } else {
+      console.log(`Nenhum usuário encontrado com o e-mail: ${userEmail}`);
+      return { success: false, message: 'Usuário não encontrado' };
     }
+  } catch (err) {
+    console.error('Erro ao gravar no banco de dados:', err);
+    return { success: false, message: 'Erro ao integrar com o Adquirente' };
+  } finally {
+    await client.end();
+  }
 }
 
 module.exports = { atualizarTokenBytepay };
